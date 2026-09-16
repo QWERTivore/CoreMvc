789486ea516dde3b9a81ab1ecdc036f6015b9a1a
Cody.A <qwertivore@gmail.com>
2026-09-15 19:46:28 -0700

    [View Products] updated property naming convention in viewmodel to better express the view contract. trivial modification in the view as i plan how to implement the filter and the sort for the grid data

--- Presentation/ViewModels/ProductListViewModel.cs
+++ Presentation/ViewModels/ProductListViewModel.cs
@@ -5,9 +5,9 @@ namespace GetTheRepsWeb.Presentation.ViewModels
     public class ProductListViewModel
     {
         public List<ProductViewModel> Products { get; set; }
-        public List<ProductCategory> Categories { get; set; }
+        public List<ProductCategory> CategoryFilter { get; set; }
         public ProductCategory SelectedCategory { get; set; }
         public List<SortingOption> SelectedSortingOption { get; set; }
-        public int MaxItems { get; set; }
+        public int MaxProductsDisplayed { get; set; }
     }
 }
