83cf86dceb6a10398a84cd81c0d6a7851f0faa83
Cody.A <qwertivore@gmail.com>
2026-09-16 18:57:52 -0700

    [View Product] i added the rating model because it is going to be its own table in the database

--- Persistance/Models/RatingModel.cs
+++ Persistance/Models/RatingModel.cs
@@ -2,5 +2,9 @@
 {
     public class RatingModel
     {
+        public int Id { get; set; }
+        public int Stars { get; set; }
+        public int ProductId { get; set; }
+        public ProductModel Product { get; set; }
     }
 }
