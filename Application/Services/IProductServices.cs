816e2887f860e5b56fe264a9fe2a2969481b736c
Cody.A <qwertivore@gmail.com>
2026-09-17 19:00:22 -0700

    [View Product] fully implemented the product service interface

--- Application/Services/IProductServices.cs
+++ Application/Services/IProductServices.cs
@@ -1,12 +1,10 @@
-﻿namespace GetTheRepsWeb.Application.Services
+﻿using GetTheRepsWeb.Application.DTO;
+using GetTheRepsWeb.Domain.Entities;
+
+namespace GetTheRepsWeb.Application.Services
 {
     public interface IProductServices
     {
-        // inspect the use case tiles 
-
-        // im thinking that we should use a exception handling... but what kind? this is certainly a testable
-        // piece of code. so we need to be able to simulate the data not being found, the query being invalid.... hmmm what else?
-
-        // call the repository and tell it what we need, this obviously needs to be async....
+        public Task<IReadOnlyCollection<Product>> ViewProductsAsync(ViewProductDto query);
     }
 }
--- Application/Services/ProductPageService.cs
+++ Application/Services/ProductPageService.cs
@@ -2,5 +2,11 @@
 {
     public class ProductPageService
     {
+        // the service accepts a dto
+        // the service maps the use-case
+        // the service uses the repository
+        // the repository returns domain entities
+        // the service returns domain entities to the controller
+        // the controller maps the domain to the view model
     }
 }
