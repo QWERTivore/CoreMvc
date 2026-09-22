8666cc462ba6a914e97fbf66c70e83568d090408
Cody.A <qwertivore@gmail.com>
2026-09-21 20:04:21 -0700

    [Domain Change] i implemented the contract for the event Inspection. the way i am thinking about is that event types have events, and that the events themselves will duse delegates to instruct the schedule how it needs to manage the interaction with the maintenance type. schedules responsibility is purely time, milage, and the placement of evet types on those timelines. when an action is taken against that event type, sometimes the event needs to instruct the schedule to talk to the maintenance book how to proceed. so that is what is reflected here, on create irregular, the inspection event will instruct the schedule to call its irregular method, and that method should fine the next occurance of the event type on the timeline, then ask the maintenance book what to do. the maintenance book will tell it to place a new event type on the time line, and then the event will tell it to mark it as active

--- Domain/MaintenanceBook/Events/Inspection.cs
+++ Domain/MaintenanceBook/Events/Inspection.cs
@@ -1,4 +1,6 @@
 ﻿
+using System.ComponentModel;
+
 namespace GetTheRepsWeb.Domain.MaintenanceBook.Events
 {
     public class Inspection : Category
@@ -7,5 +9,16 @@ namespace GetTheRepsWeb.Domain.MaintenanceBook.Events
         public override bool RequiresFollowUp => false;
         public override bool IsInspection => true;
 
+        public override Action<Schedule, Category> CreateIrregular => 
+            (schedule, category) => {
+                var inspectionEvent = schedule.MarkInspectionAsIrregular(category);
+                schedule.MarkEventOpen(inspectionEvent);
+            };
+        public override Action<Schedule, Category> MarkComplete =>
+            (schedule, category) => {
+                schedule.MarkInspectionAsComplete(category);
+            };
+        public override Action<Schedule, Category> ResetMaintenance => (schedule, category) => { };
+        public override Action<Schedule, Category> CreateMaintenance => (schedule, category) => { };
     }
 }
